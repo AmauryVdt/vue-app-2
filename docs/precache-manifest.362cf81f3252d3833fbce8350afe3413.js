9569be3b37d7cb39a15500c7f83cfa582cd0985a
AmauryVdt <amaury.vdt@gmail.com>
2021-03-15 08:13:47 +0100

    deploy docs

---
+++ docs/precache-manifest.362cf81f3252d3833fbce8350afe3413.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "79e2b86f19d1cde3a01a",
+    "url": "/vue-app-2/css/about.0e433876.css"
+  },
+  {
+    "revision": "18b83c034b45b4164012",
+    "url": "/vue-app-2/css/app.071259b2.css"
+  },
+  {
+    "revision": "a4b95650265b0170974f615b1c219de4",
+    "url": "/vue-app-2/img/wikiwars_logo.a4b95650.png"
+  },
+  {
+    "revision": "eb4bf553e574a6d9d5091a8ea584eee9",
+    "url": "/vue-app-2/index.html"
+  },
+  {
+    "revision": "79e2b86f19d1cde3a01a",
+    "url": "/vue-app-2/js/about.8ab333db.js"
+  },
+  {
+    "revision": "18b83c034b45b4164012",
+    "url": "/vue-app-2/js/app.d12d6a86.js"
+  },
+  {
+    "revision": "f077cadcb110dc8da505",
+    "url": "/vue-app-2/js/chunk-vendors.fc010b69.js"
+  },
+  {
+    "revision": "a20c75afa1f0a984f0883b71519b2755",
+    "url": "/vue-app-2/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/vue-app-2/robots.txt"
+  }
+]);
