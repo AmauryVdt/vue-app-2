8599ddbb64bc2e0197e780fd1961ff6e43104c7a
AmauryVdt <amaury.vdt@gmail.com>
2021-03-15 08:34:23 +0100

    change the vue.config.js file, add outputDir

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   lintOnSave: false,
-  publicPath: process.env.NODE_ENV === "production" ? '/vue-app-2/' : '/'
+  outputDir: 'docs',
+  publicPath: process.env.NODE_ENV === "production"
+      ? '/vue-app-2/'
+      : '/'
 }
